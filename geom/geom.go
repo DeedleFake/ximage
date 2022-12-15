ded15920869bd0ad4185f223aff3efd527362200
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-14 23:53:41 -0500

    geom: add
    This package was basically just extracted from `deedles.dev/kawa/geom`.
    It isn't particularly specific to that project, so I think that it makes
    more sense here.

---
+++ geom/geom.go
@@ -0,0 +1,20 @@
+package geom
+
+import "golang.org/x/exp/constraints"
+
+// Scalar is a constraint for the types that geom types and functions
+// can handle.
+type Scalar interface {
+	constraints.Integer | constraints.Float
+}
+
+// Edges is a bitmask representing zero or more edges of a rectangle.
+type Edges uint32
+
+const (
+	EdgeNone Edges = 0
+	EdgeTop  Edges = 1 << (iota - 1)
+	EdgeBottom
+	EdgeLeft
+	EdgeRight
+)
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module deedles.dev/ximage
 
 go 1.19
 
-require github.com/stretchr/testify v1.8.1
+require (
+	github.com/stretchr/testify v1.8.1
+	golang.org/x/exp v0.0.0-20221212164502-fae10dda9338
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
